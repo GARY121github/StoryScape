3081069156ac3ae87d5fe5a8ea627a43765efd4d
GARY <108233340+GARY121github@users.noreply.github.com>
2023-11-04 13:40:22 +0530

    Store + Appwrite setup created

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './index.css'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return loading ?
+    (
+      <h1>Loading...</h1>
+    ) :
+    (
+      <div className="">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+}
+
+export default App
