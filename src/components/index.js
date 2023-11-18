e77f03d9a5f69cb33963046c4599108951296ed3
GARY <108233340+GARY121github@users.noreply.github.com>
2023-11-19 01:40:15 +0530

    Created PostCard , AuthLayout( for protection ) and Login & Signup(from react-hook-form)

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,23 @@ import Footer from './Footer/Footer';
 import Container from './Container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
-
+import Input from './Input';
+import Button from './Button';
+import Select from './Select';
+import Login from './Login';
+import Signup from './Signup';
+import PostCard from './PostCard';
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button,
+    Select,
+    Login,
+    Signup,
+    PostCard,
 }
