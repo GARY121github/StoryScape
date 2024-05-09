affb76269e1fe4f0edb2b4587b59bdc03914d9d9
gary <harshprakash000@gmail.com>
2024-05-09 12:30:06 +0530

    Changed Name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({ width = '100px' }) {
     return (
-        <h1 className="font-bold text-2xl">BLOGGING WEBSITE</h1>
+        <h1 className="font-bold text-2xl">Story Space</h1>
     )
 }
 
